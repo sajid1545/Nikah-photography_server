a6a9d5fa32d67f564594a5d7eb0c41062c91fd63
sajid9962 <clashking1545@gmail.com>
2022-11-08 01:33:22 +0600

    database connected

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
+
+
 
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yiwjyul.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+console.log(uri);
 
 app.get('/', (req, res) => {
 	res.send('Assignment 11 server running');
