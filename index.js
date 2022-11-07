891c01b0c3fbf32d5af3983e7e68745bb9c67fbc
sajid9962 <clashking1545@gmail.com>
2022-11-08 04:36:01 +0600

    api for adding reviews is added, reviews will be displayed in descending order

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -23,17 +23,55 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		const serviceCollection = client.db('nikahPhotography').collection('services');
+		const reviewCollection = client.db('nikahPhotography').collection('reviews');
 
-        // creating services to database
+		// creating services to database
 		app.post('/services', async (req, res) => {
 			const service = req.body;
 			// console.log(service);
 			const result = await serviceCollection.insertOne(service);
 			res.send(result);
-        });
-        
-        // reading services from database
+		});
 
+		// reading only 3 services from database
+		app.get('/limited-services', async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.limit(3).toArray();
+			res.send(services);
+		});
+
+		// reading only 3 services from database
+		app.get('/services', async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.toArray();
+			res.send(services);
+		});
+
+		// reading only single service from database
+		app.get('/services/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const service = await serviceCollection.findOne(query);
+			res.send(service);
+		});
+
+		// creating reviews
+
+		app.post('/reviews', async (req, res) => {
+			const review = req.body;
+			console.log([{ review, time: new Date() }]);
+			const result = await reviewCollection.insertMany([{ review: review, time: new Date() }]);
+			res.send(result);
+		});
+
+		app.get('/reviews', async (req, res) => {
+			const query = {};
+			const cursor = reviewCollection.find(query).sort({ time: -1 });
+			const reviews = await cursor.toArray();
+			res.send(reviews);
+		});
 	} finally {
 	}
 }
