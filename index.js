455a0eb862c3a6345f4349b53a5e32ea02a0e364
sajid9962 <clashking1545@gmail.com>
2022-11-08 22:48:10 +0600

    api for deleting review created

--- index.js
+++ index.js
@@ -112,6 +112,15 @@ async function run() {
 			const result = await reviewCollection.updateOne(query, updatedReview);
 			res.send(result);
 		});
+
+		/// deleting review from database
+
+		app.delete('/reviews/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await reviewCollection.deleteOne(query);
+			res.send(result);
+		});
 	} finally {
 	}
 }
