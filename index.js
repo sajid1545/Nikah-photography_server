2f32ceb8ef0f5d61e3ca794e887b8699e57ebca1
sajid9962 <clashking1545@gmail.com>
2022-11-09 02:56:28 +0600

    error status changed

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function verifyJWT(req, res, next) {
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
 		if (err) {
-			return res.status(401).send({ message: 'Invalid authorization ' });
+			return res.status(403).send({ message: 'Forbidden Access ' });
 		}
 		req.decoded = decoded;
 		next();
